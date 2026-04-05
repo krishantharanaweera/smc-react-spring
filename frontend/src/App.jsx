098b4c4e2f44e75911b033cfd031f805dd801f66
Krishan Ranaweera <krishantha7470846@gmail.com>
2026-04-05 17:13:38 +0530

    resrouces frontend created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Toaster } from 'react-hot-toast'
 import { AuthProvider, useAuth } from './context/AuthContext'
 import Navbar from './components/NavBar'
 // import Dashboard from './pages/Dashboard'
-// import ResourcesPage from './pages/ResourcesPage'
+import ResourcesPage from './pages/ResourcesPage'
 // import BookingsPage from './pages/BookingsPage'
 // import TicketsPage from './pages/TicketsPage'
 // import NotificationsPage from './pages/NotificationsPage'
@@ -19,12 +19,14 @@ function AppLayout() {
       <Navbar />
       <main className="main-content">
         <Routes>
-          {/* <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/resources" element={<ResourcesPage />} />
-          <Route path="/bookings" element={<BookingsPage />} />
-          <Route path="/tickets" element={<TicketsPage />} />
-          <Route path="/notifications" element={<NotificationsPage />} />
-          <Route path="*" element={<Navigate to="/dashboard" replace />} /> */}
+          {
+            //<Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/resources" element={<ResourcesPage />} />
+            // <Route path="/bookings" element={<BookingsPage />} />
+            // <Route path="/tickets" element={<TicketsPage />} />
+            // <Route path="/notifications" element={<NotificationsPage />} />
+            // <Route path="*" element={<Navigate to="/dashboard" replace />} /> 
+          }
         </Routes>
       </main>
     </div>
